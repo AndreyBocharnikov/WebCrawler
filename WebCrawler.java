3ea1cb9c7b29a9dcd8c67bc811d6543c53f9003b
AndreyBocharnikov <a-bocharnikov@bk.ru>
2020-04-07 19:56:30 +0300

    bag fix

--- WebCrawler.java
+++ WebCrawler.java
@@ -5,7 +5,7 @@ import java.net.MalformedURLException;
 import java.util.*;
 import java.util.concurrent.*;
 
-import static crawler.URLUtils.getHost;
+import static info.kgeorgiy.java.advanced.crawler.URLUtils.getHost;
 
 public class WebCrawler implements Crawler {
     private final static String USAGE = "Usage: WebCrawler url [depth [downloads [extractors [perHost]]]]. Default value for all params os 1";
@@ -13,44 +13,43 @@ public class WebCrawler implements Crawler {
     private final int perHost;
     private final ExecutorService executorDownloads;
     private final ExecutorService executorExtractors;
-    private ConcurrentMap<String, TaskBufferForHost> taskBufferForHostMap;
+    private ConcurrentMap<String, TaskBufferForHost> taskBufferForHost;
 
-    public WebCrawler(Downloader downloader, int downloaders, int extractors, int perHost) {
+    public WebCrawler(final Downloader downloader, final int downloaders, final int extractors, final int perHost) {
         this.downloader = downloader;
         this.perHost = perHost;
         executorDownloads = Executors.newFixedThreadPool(downloaders);
         executorExtractors = Executors.newFixedThreadPool(extractors);
-        taskBufferForHostMap = new ConcurrentHashMap<>();
+        taskBufferForHost = new ConcurrentHashMap<>();
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         if (args.length == 0 || args.length > 5) {
             System.out.println(USAGE);
             return;
         }
 
         try {
-            int depth = Integer.decode(getArgument(args, 1));
-            int dowloaders = Integer.decode(getArgument(args, 2));
-            int extractors = Integer.decode(getArgument(args, 3));
-            int perHost = Integer.decode(getArgument(args, 4));
+            final int depth = Integer.parseInt(getArgument(args, 1));
+            final int dowloaders = Integer.parseInt(getArgument(args, 2));
+            final int extractors = Integer.parseInt(getArgument(args, 3));
+            final int perHost = Integer.parseInt(getArgument(args, 4));
 
-            WebCrawler webCrawler = new WebCrawler(new CachingDownloader(), dowloaders, extractors, perHost);
-            Result res = webCrawler.download(args[0], depth);
-            List<String> down = res.getDownloaded();
-            for (String d : down) {
-                System.out.println(d);
+            try (final WebCrawler webCrawler = new WebCrawler(new CachingDownloader(), dowloaders, extractors, perHost)) {
+                final Result res = webCrawler.download(args[0], depth);
+                final List<String> down = res.getDownloaded();
+                for (final String d : down) {
+                    System.out.println(d);
+                }
             }
-            webCrawler.close();
-
-        } catch (IOException e) {
+        } catch (final IOException e) {
             System.err.println("Couldn't initialize downloader: " + e.getMessage());
-        } catch (NumberFormatException e) {
+        } catch (final NumberFormatException e) {
             System.err.println("Expected numeric arguments only\n" + USAGE);
         }
     }
 
-    static private String getArgument(String[] args, int index) {
+    static private String getArgument(final String[] args, final int index) {
         if (index >= args.length) {
             return "1";
         } else {
@@ -60,114 +59,99 @@ public class WebCrawler implements Crawler {
 
     private class TaskBufferForHost {
         private int currentTasks;
-        private Queue<Runnable> tasks;
+        private final Queue<Runnable> tasks = new ArrayDeque<>();
 
-        TaskBufferForHost() {
-            currentTasks = 0;
-            tasks = new ArrayDeque<>();
-        }
-
-        synchronized void runTask(boolean finished) {
-            if (finished) {
-                currentTasks--;
-            }
-
-            if (currentTasks < perHost) {
-                Runnable task = tasks.poll();
-                if (task == null) {
-                    return;
-                }
+        synchronized void runTask() {
+            if (currentTasks < perHost && !tasks.isEmpty()) {
+                final Runnable task = tasks.poll();
                 currentTasks++;
                 executorDownloads.submit(() -> {
                     try {
                         task.run();
                     } finally {
-                        runTask(true);
+                        currentTasks--;
+                        runTask();
                     }
                 });
             }
         }
 
-        synchronized void addTask(Runnable task) {
+        synchronized void addTask(final Runnable task) {
             tasks.add(task);
-            runTask(false);
+            runTask();
         }
-
     }
 
     private class Solver {
         Queue<String> queue = new ConcurrentLinkedQueue<>();
+        Queue<String> queueCopy = new ConcurrentLinkedQueue<>();
         Set<String> unique = new HashSet<>();
         Queue<String> result = new ConcurrentLinkedQueue<>();
         ConcurrentMap<String, IOException> errors = new ConcurrentHashMap<>();
-        Phaser bfsDone = new Phaser(1);
 
-        Solver(String url, int depth) {
+        Solver(final String url) {
             queue.add(url);
-            run(depth);
         }
 
-        void run(int depth) {
-            bfsDone.register();
+        Result run(final int depth) {
             for (int i = 0; i < depth; i++) {
-                List<String> temp = List.copyOf(queue);
+                Queue<String> queueTmp = queue;
+                queue = queueCopy;
+                queueCopy = queueTmp;
+
                 final Phaser layer = new Phaser(1);
                 queue.clear();
-                int finalI = i;
-                temp.stream().filter(unique::add).forEach(url -> download(url, depth - finalI, layer));
+                final int finalI = i;
+                queueCopy.stream()
+                        .filter(unique::add)
+                        .forEach(url -> download(url, depth - finalI, layer));
                 layer.arriveAndAwaitAdvance();
             }
-            bfsDone.arrive();
+            return new Result(new ArrayList<>(result), errors);
         }
 
-        private void download(String url, int depthLeft, Phaser lock) {
-            String host;
+        private void download(final String url, final int depthLeft, final Phaser phaser) {
+            final String host;
             try {
                 host = getHost(url);
-            } catch (MalformedURLException e) {
+            } catch (final MalformedURLException e) {
                 errors.put(url, e);
                 return;
             }
 
-            lock.register();
-            TaskBufferForHost currentHostTask = taskBufferForHostMap.computeIfAbsent(host, s -> new TaskBufferForHost());
-            currentHostTask.addTask(() -> {
+            phaser.register();
+            taskBufferForHost.computeIfAbsent(host, s -> new TaskBufferForHost()).addTask(() -> {
                 try {
-                    Document document = downloader.download(url);
+                    final Document document = downloader.download(url);
                     result.add(url);
                     if (depthLeft > 1) {
-                        extract(document, lock);
+                        extract(document, phaser);
                     }
-                } catch (IOException e) {
+                } catch (final IOException e) {
                     errors.put(url, e);
                 } finally {
-                    lock.arrive();
+                    phaser.arrive();
                 }
             });
         }
 
-        private void extract(final Document document, Phaser lock) {
+        private void extract(final Document document, final Phaser lock) {
             lock.register();
             executorExtractors.submit(() -> {
                 try {
-                    List<String> currentResult = document.extractLinks();
-                    queue.addAll(currentResult);
-                } catch (IOException ignored) {
+                    queue.addAll(document.extractLinks());
+                } catch (final IOException ignored) {
                 } finally {
                     lock.arrive();
                 }
             });
         }
 
-        Result getAnswer() {
-            bfsDone.arriveAndAwaitAdvance();
-            return new Result(new ArrayList<>(result), errors);
-        }
     }
 
     @Override
-    public Result download(String url, int depth) {
-        return new Solver(url, depth).getAnswer();
+    public Result download(final String url, final int depth) {
+        return new Solver(url).run(depth);
     }
 
     @Override
@@ -177,8 +161,9 @@ public class WebCrawler implements Crawler {
         try {
             executorExtractors.awaitTermination(0, TimeUnit.MILLISECONDS);
             executorDownloads.awaitTermination(0, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException e) {
+        } catch (final InterruptedException e) {
             System.err.println("Could not terminate executor pools: " + e.getMessage());
         }
     }
 }
+
